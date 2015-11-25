4e5234c9093722f194354ecab0dd7bea0c71a7bd
Stephen LaPorte <stephen.laporte@gmail.com>
2015-11-24 23:58:27 -0800

    Unfortunately necessary

---
+++ word_filter.py
@@ -0,0 +1,19 @@
+
+WORD_FILTERS = ['penis',
+                'sex',
+                'masturbat',
+                'missionary']
+
+
+def word_filter(text):
+    words = text.replace('_', ' ').replace('-', ' ').split()
+    for word in words:
+        for word_filter in WORD_FILTERS:
+            if word_filter.lower() in word.lower():
+                return True
+    return False
+
+
+if __name__ == '__main__':
+    import pdb
+    pdb.set_trace()
